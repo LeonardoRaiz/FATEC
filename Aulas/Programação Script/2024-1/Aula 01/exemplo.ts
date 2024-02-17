465104812fed1f2ac9c2b4b355c66dccbcd1e576
LeonardoRaiz <leonardoraiz@icloud.com>
2024-02-17 11:16:25 -0300

    Aulas

---
+++ Aulas/Programação Script/2024-1/Aula 01/exemplo.ts
@@ -0,0 +1,13 @@
+namespace HelloWorld
+{
+    //ESCREVA
+    console.log("Olá Mundo!!!");
+
+    //var - public let - private const - constante
+    var status: string = "Atacou";
+    let qtd: number = 5;
+
+    console.log(`O boss ${status} ${qtd} vezes.`)
+    let vidaHeroi: number = 100
+    console.log(`O heroi ficou ${vidaHeroi - (qtd * 2)} de vida`)
+}
