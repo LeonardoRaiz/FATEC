837aa510e425b12679c689ddbb5df9dffc258bfc
LeonardoRaiz <leonardoraiz@icloud.com>
2024-03-22 20:42:16 -0300

    Aulas

--- Aulas/Desenvolvimento Web II/2024-1/Aula 09/index.js
+++ Aulas/Desenvolvimento Web II/2024-1/Aula 09/index.js
@@ -5,11 +5,15 @@ createApp({
         return {
             firstName: '',
             lastName: '',
+            status: false
         }
     },
     methods: {
         fullName() {
             return (this.firstName + " " + this.lastName)
+        },
+        onOff() {
+            return this.status = !this.status
         }
     }
 }).mount("#app");
