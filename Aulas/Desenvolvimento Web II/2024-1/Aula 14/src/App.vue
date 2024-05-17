af0a3afb8f8a1d050ccd1d23d215474452921dfd
Leonardo <lhraiz@cpqd.com.br>
2024-05-17 20:40:59 -0300

    SPA

---
+++ Aulas/Desenvolvimento Web II/2024-1/Aula 14/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <HelloWorld msg="Esta é a nossa Primeira PROPS"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Aulas/Desenvolvimento Web II/2024-1/Aula 14/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <p>Olá mundo</p>
+  <h1 class="texto">{{ msg }}</h1>
+</template>
+
+<style scoped>
+.texto {
+  color: red;
+  font-style: italic;
+}
+</style>
