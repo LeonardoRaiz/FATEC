137f51888942fe49b184017c6408b707ebad6273
leonardoraiz <leonardoraiz@icloud.com>
2024-10-07 22:09:41 -0300

    Aula 07/10

--- Aulas/Programação Script/2024-2/Aula 06/index.js
+++ Aulas/Programação Script/2024-2/Aula 06/index.js
@@ -9,15 +9,27 @@ createApp({
             nextPage: 1,
         }
     },
+    created() {
+        this.callAPI();
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
     methods: {
         async callAPI() {
             try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${this.nextPage - 1}&limit=${151}`)
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${(this.nextPage - 1) * 151}&limit=${151}`)
                 const data = await response.json();
                 const pokemonDetailsPromises = data.results.map(async pokemon => this.fetchPokemonData(pokemon.url));
-                console.log(pokemonDetailsPromises)
+                //console.log(pokemonDetailsPromises)
                 const pokemonDetails = await Promise.all(pokemonDetailsPromises);
-                console.log(pokemonDetails)
+                //console.log(pokemonDetails)\
+                this.pokemons = [...this.pokemons, ...pokemonDetails];
+                this.nextPage++;
+                //console.log(this.pokemons)
+                this.loading = false;
+
             } catch (error) {
                 console.error(error);
             }
@@ -27,10 +39,25 @@ createApp({
             try {
                 const response = await fetch(url);
                 const data = await response.json();
-                return {data}
+                return {
+                    id: data.id,
+                    name: data.name,
+                    weight: data.weight,
+                    types: data.types,
+                    sprites: data.sprites,
+                    showDetails: false,
+                }
             } catch (e){
                 console.error(e);
             }
+        },
+        handleScroll() {
+            const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+            if (bottomOfWindow && !this.loading) {
+                this.loading = true;
+                this.callAPI();
+            }
+
         }
     }
 }).mount("#app");
