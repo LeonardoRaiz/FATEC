28d510df886d1f6f1c5e94bd8cd6e8b7782b6129
leonardoraiz <leonardoraiz@icloud.com>
2025-09-08 21:19:28 -0300

    aulas

--- Aulas/Programação Script/2025-2/aula-03/app/page.js
+++ Aulas/Programação Script/2025-2/aula-03/app/page.js
@@ -6,18 +6,24 @@ export default function Home() {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
     const [valor, setValor] = useState(0);
+    const [texto, setTexto] = useState(0);
+    const [texto2, setTexto2] = useState(0);
 
     function handleMouseMove(event) {
         setX(event.clientX);
         setY(event.clientY);
     }
 
-    function handleClick(teste) {
-        if (teste === "+") {
+    function handleClick(op) {
+        if (op === '+') {
             setValor(valor + 1);
         } else {
             setValor(valor - 1);
         }
+    }
+
+    function equal() {
+        setValor(parseInt(texto) + parseInt(texto2));
     }
 
     return (
@@ -27,6 +33,11 @@ export default function Home() {
             <span>Número: {valor}</span>
             <button onClick={() => handleClick("+")}> + 1</button>
             <button onClick={() => handleClick("-")}> - 1</button>
+            <input onChange={(event) => setTexto(event.target.value)} />
+            <span>{texto}</span>
+            <input onChange={(event) => setTexto2(event.target.value)} />
+            <span>{texto2}</span>
+            <button onClick={() => equal()}> =</button>
         </div>
     );
 }
