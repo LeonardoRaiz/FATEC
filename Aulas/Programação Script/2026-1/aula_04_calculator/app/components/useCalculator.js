b42f169fbea7b53a1616fea5a65c6e16cd462598
Leonardo Henrique Raiz <lhraiz@cpqd.com.br>
2026-03-30 22:10:13 -0300

    Calculator

--- Aulas/Programação Script/2026-1/aula_04_calculator/app/components/useCalculator.js
+++ Aulas/Programação Script/2026-1/aula_04_calculator/app/components/useCalculator.js
@@ -10,14 +10,60 @@ export default function useCalculator() {
             handleNumber(value)
         } else if (value === ".") {
             console.log("Clicou no decimal");
+            handleDecimal()
         } else {
-            console.log("Clicou no operador" + value);
+            handleOperator(value)
         }
     }
 
     function handleNumber(value) {
         setDisplayValue((prev) =>
             prev === "0" ? String(value) : prev + String(value));
+    }
+    function handleDecimal() {
+        setDisplayValue((prev) => {
+            if(!prev.includes(".")){
+                return prev + "."
+            }
+            return prev;
+        });
+    }
+
+    function handleOperator(op) {
+        if(op === "AC"){
+            clear();
+        } else if (op === "+/-"){
+            setDisplayValue((prev) =>
+                String(-1 * parseFloat(prev)));
+        } else if (op === "%"){
+            setDisplayValue((prev) =>
+                String(parseFloat(prev) / 100));
+        } else if (op === "="){
+            if(previousValue !== null && op){
+                const result = calculate(previousValue,
+                    displayValue, operator);
+                setDisplayValue(String(result));
+                setPreviousValue(null);
+                setOperator(null);
+            }
+        } else {
+            //TODO
+            //PODE TER ALGO!?
+            setPreviousValue(displayValue);
+            setOperator(op)
+            setDisplayValue("0");
+        }
+    }
+    function calculate(previousValue, displayValue, operator) {
+        const num1 = parseFloat(previousValue);
+        const num2 = parseFloat(displayValue);
+
+        if (operator === "+") return num1 + num2;
+        if (operator === "-") return num1 - num2;
+        if (operator === "*") return num1 * num2;
+        if (operator === "/") return num1 / num2;
+
+        return num2;
     }
     return {
         displayValue,
